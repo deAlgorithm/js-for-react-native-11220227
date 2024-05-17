a06d14a480aa0c025a77e48dfd08225a2a19986f
Otabil Ishaque <iotabil@st.ug.edu.gh>
2024-05-17 15:58:20 +0100

    Completed task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,6 +1,12 @@
-function processArray(myArray)  {
+function processArray(myArray) {
     return myArray.map(
-        number => number % 2 == 0 ? number * 2 : number * 3
+        number => number % 2 === 0 ? number * 2 : number * 3
     );
-    
 }
+function formatArrayStrings (strings, numbers) {
+
+    return strings.map((str, index) => 
+        (numbers[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase())
+    );
+};
+
